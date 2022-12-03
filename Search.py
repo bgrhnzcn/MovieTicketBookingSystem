3a2ec75db8fbbe719d050142843e912fc2b04969
Bugrahan Ozcan <bugrahanozcan16@gmail.com>
2022-12-03 22:08:12 +0300

    Base files for created classes.

---
+++ Search.py
@@ -0,0 +1,15 @@
+class Search:
+    def search_by_title(self):
+        pass
+
+    def search_by_language(self):
+        pass
+
+    def search_by_genre(self):
+        pass
+
+    def search_by_release_date(self):
+        pass
+
+    def search_by_city(self):
+        pass
